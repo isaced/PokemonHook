c888e958edef26f83c5d925e2c03b7fda6c149a0
isaced <isaced@163.com>
2016-07-12 01:29:29 +0800

    update pack script

--- gogo.sh
+++ gogo.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
 # clean
-rm -rf "Payload"
 rm pokemon_hook.ipa
 
 # unzip
 unzip -q "pokemon_unsigned.zip"
 
-# remove this
-rm -rf "__MACOSX"
 
 # cp dylib
 cp LatestBuild/LocationFaker.dylib Payload/pokemongo.app/libLocationFaker.dylib
 
 # embedded.mobileprovision
-# cp embedded.mobileprovision Payload/pokemongo.app/embedded.mobileprovision
+cp embedded.mobileprovision Payload/pokemongo.app/embedded.mobileprovision
 
 # resign
-# codesign -f -s "iPhone Distribution: Kunhua  Technology Co., Ltd. (H73L5KDA32)" Payload/pokemongo.app
+rm -rf Payload/pokemongo.app/_CodeSignature
+security cms -D -i Payload/pokemongo.app/embedded.mobileprovision > provision.plist
+/usr/libexec/PlistBuddy -x -c 'Print :Entitlements' provision.plist > entitlements.plist
+codesign -f -s "" --entitlements entitlements.plist Payload/pokemongo.app
+codesign -f -s "" --entitlements entitlements.plist Payload/pokemongo.app/*.dylib
 
 # zip to ipa
 zip -qr pokemon_hook.ipa Payload
+
+#clean
+rm -rf "Payload"
+rm provision.plist
+rm entitlements.plist
+rm -rf "__MACOSX"
 
 # --- mark ---
 # security find-identity -v -p codesigning
