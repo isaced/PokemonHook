1ee543a18bd0956d9c310ad9e15384296f279d15
isaced <isaced@163.com>
2016-07-12 15:49:42 +0800

    update pack script

--- gogo.sh
+++ gogo.sh
@@ -1,33 +1,35 @@
 #!/bin/sh
 
+# certifierName=""  
+
 # clean
-rm pokemon_hook.ipa
+rm pokemon.ipa
 
 # unzip
-unzip -q "pokemon_unsigned.zip"
+unzip -q "pokemon_unsigned.zip" #-d pack
 
 
 # cp dylib
 cp LatestBuild/LocationFaker.dylib Payload/pokemongo.app/libLocationFaker.dylib
 
 # embedded.mobileprovision
-cp embedded.mobileprovision Payload/pokemongo.app/embedded.mobileprovision
+# cp embedded.mobileprovision Payload/pokemongo.app/embedded.mobileprovision
 
 # resign
-rm -rf Payload/pokemongo.app/_CodeSignature
-security cms -D -i Payload/pokemongo.app/embedded.mobileprovision > provision.plist
-/usr/libexec/PlistBuddy -x -c 'Print :Entitlements' provision.plist > entitlements.plist
-codesign -f -s "" --entitlements entitlements.plist Payload/pokemongo.app
-codesign -f -s "" --entitlements entitlements.plist Payload/pokemongo.app/*.dylib
+# rm -rf pake/Payload/pokemongo.app/_CodeSignature
+# security cms -D -i Payload/pokemongo.app/embedded.mobileprovision > provision.plist
+# /usr/libexec/PlistBuddy -x -c 'Print :Entitlements' provision.plist > entitlements.plist
+# codesign -f -s $certifierName --entitlements entitlements.plist Payload/pokemongo.app
+# codesign -f -s $certifierName --entitlements entitlements.plist Payload/pokemongo.app/*.dylib
 
 # zip to ipa
-zip -qr pokemon_hook.ipa Payload
+zip -qr pokemon.ipa Payload
 
 #clean
-rm -rf "Payload"
-rm provision.plist
-rm entitlements.plist
-rm -rf "__MACOSX"
+rm -rf Payload
+rm -rf __MACOSX
+# rm provision.plist
+# rm entitlements.plist
 
 # --- mark ---
 # security find-identity -v -p codesigning
